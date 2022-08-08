e9c97b5df1f42008f8a3f23a4a54c593f853b589
HP <HP@LAPTOP-9R5GGNQ0>
2022-08-08 19:53:14 +0530

    First Commit

---
+++ Day6_Abstract,final ans super keyword/src/com/cg/abstractkeyworddemo/RasMalai.java
@@ -0,0 +1,10 @@
+package com.cg.abstractkeyworddemo;
+
+public abstract class RasMalai {
+
+	public RasMalai() {
+		System.out.println("FAV SWEET");
+	}
+	//abstract method
+	abstract void print();
+}
