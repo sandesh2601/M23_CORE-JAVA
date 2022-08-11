88bcf109f35ab0636e6c46f790b3e392988410d6
HP <HP@LAPTOP-9R5GGNQ0>
2022-08-11 19:13:58 +0530

    First Commit

---
+++ Day9_Interface and Wrapper Classes/src/com/cg/wrapperclass/UnBoxing.java
@@ -0,0 +1,13 @@
+package com.cg.wrapperclass;
+//program on Unboxing
+public class UnBoxing {
+
+	public static void main(String[] args) {
+		Integer a=23; //wrapper class object
+		int b=a;      //conversion of wrapper obj to primitive
+		System.out.println(a);
+		System.out.println(b);
+
+	}
+
+}
