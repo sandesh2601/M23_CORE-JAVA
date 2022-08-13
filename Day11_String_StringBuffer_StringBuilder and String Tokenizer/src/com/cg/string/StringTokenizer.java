74c8e2962f6eb2c4d82bf5b928efdb5b603d36d2
HP <HP@LAPTOP-9R5GGNQ0>
2022-08-13 09:20:04 +0530

    First Commit

---
+++ Day11_String_StringBuffer_StringBuilder and String Tokenizer/src/com/cg/string/StringTokenizer.java
@@ -0,0 +1,22 @@
+package com.cg.string;
+
+public class StringTokenizer {
+
+	public StringTokenizer(String string) {
+		// TODO Auto-generated constructor stub
+	}
+
+	public static void main(String[] args) {
+		
+		StringTokenizer st =new StringTokenizer("LETS TRY THIS");
+		System.out.println(st.nextToken());
+		System.out.println(st.nextToken());
+
+	}
+
+	private char[] nextToken() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+}
